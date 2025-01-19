eaac546b3f56e60781047c42880470bc816dfe47
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-01-19 20:18:31 +0500

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import NavBar from './Components/NavBar/Navbar'
 import "./App.css"
+import { Routes, Route } from 'react-router-dom';
+import Hero from "../src/Components/Hero/Hero"
+
+
 
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
 
+      </Routes>
 
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
 import "./index.css"
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>  {/* Wrap your app with BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
